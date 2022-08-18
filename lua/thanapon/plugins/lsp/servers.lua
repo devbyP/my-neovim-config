21da7297a769fc3a75ff50f8dbc9ae9d0a50652f
Thanapon Teerakaisri <dev.thanapon@gmail.com>
2022-08-18 21:54:11 +0700

    fixed volar tsserverlib path, and add takeover mode.

--- lua/thanapon/plugins/lsp/servers.lua
+++ lua/thanapon/plugins/lsp/servers.lua
@@ -37,11 +37,48 @@ nvim_lsp.sumneko_lua.setup({
   }
 })
 
+-- volar's config
+-- use a local tsserver, and fallback to a global server.
+local util = nvim_lsp.util
+local function get_typescript_server_path(root_dir)
+
+  -- path to tsserverlibrary
+  -- use "npm list -g"
+  -- to find where is your node_modules global package live.
+  -- replace that path with the part before "node_modules"
+  local global_ts = '/opt/homebrew/lib/node_modules/typescript/lib/tsserverlibrary.js'
+  -- Alternative location if installed as root:
+  -- local global_ts = '/usr/local/lib/node_modules/typescript/lib/tsserverlibrary.js'
+  local found_ts = ''
+  local function check_dir(path)
+    found_ts =  util.path.join(path, 'node_modules', 'typescript', 'lib', 'tsserverlibrary.js')
+    if util.path.exists(found_ts) then
+      return path
+    end
+  end
+  if util.search_ancestors(root_dir, check_dir) then
+    return found_ts
+  else
+    return global_ts
+  end
+end
+
+nvim_lsp.volar.setup({
+  flags = flags,
+  capabilities = capabilities,
+  on_attach = on_attach,
+  -- Takeover mode
+  filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'},
+  -- tsserver path option.
+  on_new_config = function(new_config, new_root_dir)
+    new_config.init_options.typescript.serverPath = get_typescript_server_path(new_root_dir)
+  end,
+})
+
 -- Default setting lsp servers.
 local servers = {
   'gopls', -- Golang
   'tsserver', -- Typescript and javascript
-  'volar', -- Vue3 recommended ls
   'html', -- HTML
   'cssls', -- Css language server
   'jsonls', -- Json
