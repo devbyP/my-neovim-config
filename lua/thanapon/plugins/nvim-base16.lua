a8e83e752ac31a515af3f078e8339f291641a056
dev.thanapon@gmail.com <dev.thanapon@gmail.com>
2022-08-19 12:47:00 +0700

    url to github repo see all colorschemes list.

--- lua/thanapon/plugins/nvim-base16.lua
+++ lua/thanapon/plugins/nvim-base16.lua
@@ -1,11 +1,13 @@
 -- COLORSCHEMES
+-- see all colorschemes list at https://github.com/RRethy/nvim-base16
 -- Uncomment just ONE of the following colorschemes!
+
 -- vim.cmd('colorscheme base16-dracular')
 -- vim.cmd('colorscheme base16-tender')
 -- vim.cmd('colorscheme base16-nord')
 -- vim.cmd('colorscheme base16-darkmos')
 -- vim.cmd('colorscheme base16-decaf')
 -- vim.cmd('colorscheme base16-ember')
--- vim.cmd('colorscheme base16-oceanicnext')
+vim.cmd('colorscheme base16-oceanicnext')
 -- vim.cmd('colorscheme base16-material-palenight')
-vim.cmd('colorscheme base16-gruvbox-dark-medium')
+-- vim.cmd('colorscheme base16-gruvbox-dark-medium')
