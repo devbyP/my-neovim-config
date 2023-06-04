5219e3358b238f3c27cf69a8fd29ba49357b1903
Thanapon Teerakaisri <dev.thanapon@gmail.com>
2023-06-04 11:52:47 +0700

    prettier use 4 tab width

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -2,6 +2,26 @@ local nls = require("null-ls")
 
 nls.setup({
     sources = {
-        nls.builtins.formatting.prettier,
+        nls.builtins.formatting.prettier.with({
+            --file_type = {
+            --    "javascript",
+            --    "javascriptreact",
+            --    "typescript",
+            --    "typescriptreact",
+            --    "vue",
+            --    "css",
+            --    "scss",
+            --    "less",
+            --    "html",
+            --    "json",
+            --    "jsonc",
+            --    "yaml",
+            --    "markdown",
+            --    "markdown.mdx",
+            --    "graphql",
+            --    "handlebars",
+            --},
+            extra_args = { "--tab-width=4" }
+        }),
     },
 })
