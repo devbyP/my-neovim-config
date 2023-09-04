f389e2a774589e01356d508e9495e7af6596dda6
Thanapon Teerakaisri <dev.thanapon@gmail.com>
2023-09-05 00:00:48 +0700

    make find files center layout

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,6 +3,11 @@ local telescope = require('telescope')
 telescope.setup({
     defaults = {
         file_ignore_patterns = { 'node_modules' }
+    },
+    pickers = {
+        find_files = {
+            theme = "dropdown",
+        }
     }
 })
 
