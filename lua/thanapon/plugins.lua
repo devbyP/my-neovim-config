cf69f80e619628ebf98191fb00cb16a10d0b41c2
Thanapon Teerakaisri <dev.thanapon@gmail.com>
2023-10-28 16:30:22 +0700

    update telescope package version

--- lua/thanapon/plugins.lua
+++ lua/thanapon/plugins.lua
@@ -38,9 +38,9 @@ return require('packer').startup(function(use)
     -- Telescope --
     use({
         'nvim-telescope/telescope.nvim',
-        tag = '0.1.0',
-        -- or                          , branch = '0.1.x',
-        requires = { { 'nvim-lua/plenary.nvim' } },
+        tag = '0.1.4',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
     })
 
     -- Harpoon for file navigation --
