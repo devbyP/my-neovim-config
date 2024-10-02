b7f72da01f6ea069f6af3afc900cdf706ef6d806
Thanapon Teerakaisri <dev.thanapon@gmail.com>
2024-10-03 02:53:31 +0700

    feat(copilot): add more key mappings for copilot chat
    Enhanced the Copilot Chat plugin by adding more key mappings:
    - Added key mappings for tests, review, commit, commit staged, explain,
      debug, fix, fix diagnostic, optimize, docs, save, load, reset, and stop.
    - Simplified the toggle function for Copilot Chat.

--- lua/me/plugins/copilot_chat.lua
+++ lua/me/plugins/copilot_chat.lua
@@ -18,37 +18,27 @@ return {
 				debug = true,
 			})
 			vim.keymap.set("n", "<leader>cc", function()
-				chat.toggle({
-					window = {
-						layout = "float",
-						title = "My Ai Copilot Chat",
-						width = 0.8,
-						height = 0.4,
-						row = 1,
-					},
-				})
-			end, { desc = "[C]opilot [C]hat Float" })
-			-- vim.keymap.set("n", "<leader>ccv", function()
-			-- 	chat.open()
-			-- end, { desc = "[C]opilot [C]hat Vertical" })
-			-- vim.keymap.set("n", "<leader>ct", ":CopilotChatTests<CR>", { desc = "Copilot Chat Tests Suggestion" })
-			-- vim.keymap.set({ "n", "v" }, "<leader>cr", ":CopilotChatReview<CR>", { desc = "Copilot Chat Review" })
-			-- vim.keymap.set("n", "<leader>cs", ":CopilotChatCommit<CR>", { desc = "Copilot Chat Commit" })
-			-- vim.keymap.set("n", "<leader>cS", ":CopilotChatCommitStaged<CR>", { desc = "Copilot Chat Commit Staged" })
-			-- vim.keymap.set({ "n", "v" }, "<leader>ce", ":CopilotChatExplain<CR>", { desc = "Copilot Chat Commit" })
-			-- vim.keymap.set("n", "<leader>cD", ":CopilotChatDebugInfo<CR>", { desc = "Copilot Chat Debug" })
-			-- vim.keymap.set({ "n", "v" }, "<leader>cf", ":CopilotChatFix<CR>", { desc = "Copilot Chat Fix" })
-			-- vim.keymap.set(
-			-- 	"n",
-			-- 	"<leader>cfd",
-			-- 	":CopilotChatFixDiagnostic<CR>",
-			-- 	{ desc = "Copilot Chat Fix Diagnostic" }
-			-- )
-			-- vim.keymap.set({ "n", "v" }, "<leader>co", ":CopilotChatOptimize<CR>", { desc = "Copilot Chat Optimize" })
-			-- vim.keymap.set({ "n", "v" }, "<leader>cd", ":CopilotChatDocs<CR>", { desc = "Copilot Chat Docs" })
-			-- vim.keymap.set("n", "<leader>ccs", ":CopilotChatSave", { desc = "Copilot Chat Save to file" })
-			-- vim.keymap.set("n", "<leader>ccl", ":CopilotChatLoad", { desc = "Copilot Chat Load file" })
-			-- vim.keymap.set("n", "<leader>ccr", ":CopilotChatReset", { desc = "Copilot Chat Reset Current chat" })
+				chat.toggle()
+			end, { desc = "[C]opilot [C]hat" })
+			vim.keymap.set("n", "<leader>ct", ":CopilotChatTests<CR>", { desc = "Copilot Chat Tests Suggestion" })
+			vim.keymap.set({ "n", "v" }, "<leader>cr", ":CopilotChatReview<CR>", { desc = "Copilot Chat Review" })
+			vim.keymap.set("n", "<leader>cs", ":CopilotChatCommit<CR>", { desc = "Copilot Chat Commit" })
+			vim.keymap.set("n", "<leader>cS", ":CopilotChatCommitStaged<CR>", { desc = "Copilot Chat Commit Staged" })
+			vim.keymap.set({ "n", "v" }, "<leader>ce", ":CopilotChatExplain<CR>", { desc = "Copilot Chat Commit" })
+			vim.keymap.set("n", "<leader>cD", ":CopilotChatDebugInfo<CR>", { desc = "Copilot Chat Debug" })
+			vim.keymap.set({ "n", "v" }, "<leader>cf", ":CopilotChatFix<CR>", { desc = "Copilot Chat Fix" })
+			vim.keymap.set(
+				"n",
+				"<leader>cfd",
+				":CopilotChatFixDiagnostic<CR>",
+				{ desc = "Copilot Chat Fix Diagnostic" }
+			)
+			vim.keymap.set({ "n", "v" }, "<leader>co", ":CopilotChatOptimize<CR>", { desc = "Copilot Chat Optimize" })
+			vim.keymap.set({ "n", "v" }, "<leader>cd", ":CopilotChatDocs<CR>", { desc = "Copilot Chat Docs" })
+			vim.keymap.set("n", "<leader>ccs", ":CopilotChatSave ", { desc = "Copilot Chat Save to file" })
+			vim.keymap.set("n", "<leader>ccl", ":CopilotChatLoad ", { desc = "Copilot Chat Load file" })
+			vim.keymap.set("n", "<leader>ccr", ":CopilotChatReset<CR>", { desc = "Copilot Chat Reset Current chat" })
+			vim.keymap.set("n", "<leader>ais", ":CopilotChatStop<CR>", { desc = "Copilot Chat Stop current output" })
 		end,
 	},
 }
